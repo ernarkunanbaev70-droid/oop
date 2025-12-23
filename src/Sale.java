12f342775234a4c5ae42c57e2474869c80581c5f
ernarkunanbaev70-droid <ernarkunanbaev70@gmail.com>
2025-12-23 12:52:02 +0500

    Update Sale.java

--- src/Sale.java
+++ src/Sale.java
@@ -12,7 +12,21 @@ public class Sale {
         this.total = item.getPrice() * count;
     }
 
-    public double getTotal() {
-        return total;
+    public int getSaleId() { return saleId; }
+    public ClothingItem getItem() { return item; }
+    public int getCount() { return count; }
+    public double getTotal() { return total; }
+
+    public void setSaleId(int saleId) { this.saleId = saleId; }
+    public void setItem(ClothingItem item) { this.item = item; }
+    public void setCount(int count) { this.count = count; total = item.getPrice() * count; }
+
+    public void recalc() { total = item.getPrice() * count; }
+    public boolean isBigSale() { return total > 20000; }
+
+    @Override
+    public String toString() {
+        return "Sale{id=" + saleId + ", item=" + item.getName() +
+               ", count=" + count + ", total=" + total + "}";
     }
 }
