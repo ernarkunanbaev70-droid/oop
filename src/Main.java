187c546dcbe2c5a5f7b985707c0192c23b613b70
ernarkunanbaev70-droid <ernarkunanbaev70@gmail.com>
2025-12-23 13:10:18 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,15 @@ public class Main {
         ClothingItem cloth = new ClothingItem(1, "Jacket", 12000, 6);
         Customer buyer = new Customer(1, "Yernar", 40000, 0);
 
-        
         System.out.println("Initial state:");
-        System.out.println("Item: " + cloth.getName() + 
-                           ", price: " + cloth.getPrice() + 
-                           ", quantity: " + cloth.getQuantity());
-        System.out.println("Customer: " + buyer.getName() + 
-                           ", cash: " + buyer.getCash() + 
-                           ", purchases: " + buyer.getPurchases());
-
-        
+        System.out.println("Item: " + cloth.getName() +  ", price: " + cloth.getPrice() + ", quantity: " + cloth.getQuantity());
+        System.out.println("Customer: " + buyer.getName() + ", cash: " + buyer.getCash() + ", purchases: " + buyer.getPurchases());
+                            
         Sale sale = new Sale(1, cloth, 2);
 
         cloth.plusQuantity(-sale.getCount());
         buyer.spend(sale.getTotal());
 
-        
         System.out.println("\nAfter sale:");
         System.out.println("Item quantity: " + cloth.getQuantity());
         System.out.println("Customer cash: " + buyer.getCash());
