e4b085514c7bbe480ae84767ed7bb0b440e4b603
ernarkunanbaev70-droid <ernarkunanbaev70@gmail.com>
2025-12-23 12:52:55 +0500

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -12,8 +12,22 @@ public class Customer {
         this.purchases = purchases;
     }
 
-    public void spend(double sum) {
-        cash -= sum;
-        purchases++;
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public double getCash() { return cash; }
+    public int getPurchases() { return purchases; }
+
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setCash(double cash) { this.cash = cash; }
+    public void setPurchases(int purchases) { this.purchases = purchases; }
+
+    public void spend(double sum) { cash -= sum; purchases++; }
+    public boolean canBuy(double sum) { return cash >= sum; }
+
+    @Override
+    public String toString() {
+        return "Customer{id=" + id + ", name=" + name +
+               ", cash=" + cash + ", purchases=" + purchases + "}";
     }
 }
